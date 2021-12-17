ce22e3f733b6903ed0a50702ec772a8e610749f4
AssanDiomande <adiomande-ext@centreon.com>
2021-12-17 17:21:13 +0100

    Update centreonDSMStats.class.php

--- www/modules/centreon-dsm/statistics/centreonDSMStats.class.php
+++ www/modules/centreon-dsm/statistics/centreonDSMStats.class.php
@@ -34,7 +34,7 @@
  *
  */
 
-require_once __DIR__ . "/../../../class/class/centreonDB.class.php";
+require_once __DIR__ . "/../../../class/centreonDB.class.php";
 require_once __DIR__ . "/../../../class/exceptions/StatisticException.php";
 
 class CentreonDSMStats
